25bfb3b013bcdeeb546acf4edf7230d1bef3927e
ddashadyy <poletaevgena75@gmail.com>
2024-11-03 23:35:36 +0800

    first exp in vim, array header added

---
+++ containers/array.hpp
@@ -0,0 +1,195 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <cstddef>
+#include <iterator>
+#include <stdexcept>
+
+template< class T, std::size_t N > 
+struct array
+{
+    using value_type = T;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    
+    reference at( size_type pos );
+    const_reference at( size_type pos ) const;
+
+    reference operator[]( size_type pos );
+    const_reference operator[]( size_type pos ) const;
+
+    reference back();
+    const_reference back() const;
+
+    pointer data() noexcept;
+    const_pointer data() const noexcept;
+
+    iterator begin() noexcept;
+    const_iterator begin() const noexcept;
+    const_iterator cbegin() const noexcept;
+    
+    iterator end() noexcept;
+    const_iterator end() const noexcept;
+    const_iterator cend() const noexcept; 
+
+    reverse_iterator rbegin() noexcept;
+    const_reverse_iterator rbegin() const  noexcept;
+    const_reverse_iterator crbegin() const noexcept;
+
+    constexpr bool empty() const noexcept;
+    constexpr size_type size() const noexcept;
+    constexpr size_type max_size() const noexcept;
+
+    void fill( const T& value );
+    void swap( array& ohter ) noexcept;
+    
+private:
+    value_type m_data[N];
+    size_type m_size;
+};
+
+
+template< class T, std::size_t N > 
+inline array<T, N>::reference array<T, N>::at( size_type pos )
+{
+    if (pos >= size())
+        throw std::out_of_range("out of range");
+
+    return m_data[pos];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::const_reference array<T, N>::at( size_type pos ) const
+{
+    if (pos >= size())
+        throw std::out_of_range("out of range");
+
+    return m_data[pos];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::reference array<T, N>::operator[]( size_type pos )
+{
+    return m_data[pos];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::const_reference array<T, N>::operator[]( size_type pos ) const
+{
+    return m_data[pos];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::reference front()
+{
+    return m_data[0];
+}
+
+
+template< class T, std::size_t N > 
+inline array<T, N>::const_reference front() const
+{
+    return m_data[0];
+}
+
+
+template< class T, std::size_t N > 
+inline array<T, N>::reference back()
+{
+    return m_data[size() - 1];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::const_reference back() const
+{
+    return m_data[size() - 1];
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::pointer array<T, N>::data() noexcept
+{
+    return &m_data;
+}
+
+template< class T, std::size_t N > 
+inline array<T, N>::const_pointer array<T, N>::data() const noexcept
+{
+    return &m_data;
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::iterator array<T, N>::begin() noexcept
+{
+    return &m_data[0];
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::const_iterator array<T, N>::begin() const noexcept
+{
+    return &m_data[0];
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::const_iterator array<T, N>::cbegin() const noexcept
+{
+    return &m_data[0];
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::iterator array<T, N>::end() noexcept
+{
+    return &m_data[N];
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::const_iterator array<T, N>::end() const noexcept
+{
+    return &m_data[N];
+}
+
+template< class T, std::size_t N >
+inline array<T, N>::const_iterator array<T, N>::cend() const noexcept
+{
+    return &m_data[N];
+}
+
+template< class T, std::size_t N >
+constexpr bool array<T, N>::empty() const noexcept
+{
+    return size() == 0;
+}
+
+template< class T, std::size_t N >
+constexpr array<T, N>::size_type array<T, N>::size() const noexcept
+{
+    return N;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // ARRAY_HPP
--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-#include <iomanip>
 #include <iostream>
-#include <string>
 #include "containers/vector.hpp"
-
-
+#include "containers/array.hpp"
  
 template<typename T>
 std::ostream& operator<<(std::ostream& s, const vector<T>& v)
@@ -16,81 +13,6 @@ std::ostream& operator<<(std::ostream& s, const vector<T>& v)
  
 int main()
 {
-    // C++11 initializer list syntax:
-    vector<std::string> words1{"the", "frogurt", "is", "also", "cursed"};
-    std::cout << "1: " << words1;
- 
-    // words2 == words1
-    vector<std::string> words2(words1.begin(), words1.end());
-    std::cout << "2: " << words2;
- 
-    // words3 == words1
-    vector<std::string> words3(words1);
-    std::cout << "3: " << words3;
- 
-    // words4 is {"Mo", "Mo", "Mo", "Mo", "Mo"}
-    vector<std::string> words4(5, "Mo");
-    std::cout << "4: " << words4;
- 
-    auto const rg = {"cat", "cow", "crow"};
-#ifdef __cpp_lib_containers_ranges
-    std::vector<std::string> words5(std::from_range, rg); // overload (11)
-#else
-    vector<std::string> words5(rg.begin(), rg.end()); // overload (5)
-#endif
-    std::cout << "5: " << words5;
 
 	return 0;
 }
-
-
-
-
-
-
-
-//int main()
-//{
-//    std::fstream file("results.txt");
-//
-//    file << "std::set versus own_set\n";
-//    file << "insertion time \n";
-//
-//    for (int i = 0; i < 10; i++)
-//    {    
-//        auto std_set_statr = std::chrono::high_resolution_clock::now();
-//
-//        std::set<int> std_set;
-//        for (int i = 0; i < 1000000; i++)
-//        {
-//            int random_number = std::experimental::randint(0, 1000000);
-//            std_set.insert(random_number);
-//        }
-//
-//        auto std_set_stop = std::chrono::high_resolution_clock::now();
-//
-//        std::chrono::duration<double> dur1 = std_set_stop - std_set_statr;
-//        file << "std_set: " << dur1.count() << "\n";
-//
-//        auto own_set_statr = std::chrono::high_resolution_clock::now();
-//
-//        set<int> own_set;
-//        for (int i = 0; i < 1000000; i++)
-//        {
-//            int random_number = std::experimental::randint(0, 1000000);
-//            own_set.insert(random_number);
-//        }
-//
-//        auto own_set_stop = std::chrono::high_resolution_clock::now();
-//
-//        std::chrono::duration<double> dur2 = own_set_stop - own_set_statr;
-//        file << "own_set: " << dur2.count() << "\n";
-//
-//        file << " \n";
-//
-//    }
-//
-//
-// 
-//    return 0;
-//}
